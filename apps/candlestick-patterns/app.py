ca2019a070c6dcb4a3335bf20c9f0ef343619f01
Oleg Polakow <olegpolakow@gmail.com>
2021-12-09 22:02:41 +0100

    Update license

--- apps/candlestick-patterns/app.py
+++ apps/candlestick-patterns/app.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2021 Oleg Polakow. All rights reserved.
-# This code is licensed under Apache 2.0 with Commons Clause license (see LICENSE.md for details)
 
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
