cfa8b0ca6149e6fdedd282dd9ebfe815359dab79
Oleg Polakow <olegpolakow@gmail.com>
2022-01-05 00:55:28 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     install_requires=[
         'numpy>=1.16.5',
         'pandas',
-        'numba>=0.54.1',
+        'numba>=0.53.1',
         'scipy',
         'scikit-learn',
         'schedule',
