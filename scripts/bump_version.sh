86bc248720d3ed378866cd71a247680263c0fb2b
Oleg Polakow <olegpolakow@gmail.com>
2022-01-06 13:45:58 +0100

    Update version badge

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -3,4 +3,4 @@
 V=${1:?"Missing version"}
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 perl -i -pe "s/.*/__version__ = \"$V\"/ if \$.==3" ../vectorbtpro/_version.py
-curl "https://img.shields.io/static/v1\?label\=version\&message\=$V\&color\=ff69b4" -o ../assets/badges/version.svg
+curl "https://img.shields.io/badge/version-$V-ff69b4" -o ../assets/badges/version.svg
