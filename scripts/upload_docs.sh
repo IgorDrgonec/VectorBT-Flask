d25f60f6d015710b3f10ac76eaffecf5a867a1ed
Oleg Polakow <olegpolakow@gmail.com>
2022-01-06 13:51:11 +0100

    Update upload_docs.sh

--- scripts/upload_docs.sh
+++ scripts/upload_docs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-python ../docs/generate_api.py
+cd "$(dirname "${BASH_SOURCE[0]}")/../docs" || exit
+python generate_api.py
 mkdocs gh-deploy --force --config-file mkdocs.insiders.yml
+rm -rf docs/api/
+rm -rf site/
