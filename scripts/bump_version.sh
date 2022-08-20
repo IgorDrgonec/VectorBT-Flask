100f650f76d744d477d495a7e07b566e4b2bb768
Oleg Polakow <olegpolakow@gmail.com>
2022-08-20 20:49:05 +0200

    Bump version

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -3,5 +3,3 @@ cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 V=${1:?"Missing version"}
 perl -i -pe "s/.*/__version__ = \"$V\"/ if \$.==3" ../vectorbtpro/_version.py
-perl -i -pe "s/.*/              $V/ if \$.==37" ../docs/overrides/partials/header.html
-curl "https://img.shields.io/badge/version-$V-ff69b4" -o ../docs/docs/assets/badges/version.svg
