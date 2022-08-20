0bf774281fca04a2bb35d013cec77b5da8b35554
Oleg Polakow <olegpolakow@gmail.com>
2022-08-20 20:37:41 +0200

    Remove scripts

--- scripts/serve_docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "${BASH_SOURCE[0]}")/../docs" || exit
-
-pip uninstall -y pdoc_to_md
-pip install -U git+https://github.com/polakowo/pdoc-to-md.git
-python generate_api.py
-mkdocs serve
-rm -rf docs/api/
