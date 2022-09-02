471738b1592dba651e03f65093a442de7c83cf2e
Piotr Yordanov <48773775+piotryordanov@users.noreply.github.com>
2022-09-02 13:21:39 +0200

    Rewrite HA with IF

--- contrib/indicators/HeikinAshi.py
+++ contrib/indicators/HeikinAshi.py
@@ -1,77 +1,55 @@
+from types import ClassMethodDescriptorType
 from numba import njit
 import numpy as np
-import pandas as pd
+import vectorbtpro as vbt
 
 
-@njit(cache=True)
-def internal_build_ha(open, high, low, close, precision):
-    """
-    Internal, numba optimized HeikinAshi function
-    """
-    ha_open = np.full(close.shape[0], np.nan)
-    ha_high = np.full(close.shape[0], np.nan)
-    ha_low = np.full(close.shape[0], np.nan)
+@njit
+def ha_apply_func(open, high, low, close, precision=None):
+    ha_open = np.full(close.shape, np.nan)
+    ha_high = np.full(close.shape, np.nan)
+    ha_low = np.full(close.shape, np.nan)
+
     ha_close = (open + high + low + close) / 4
-    np.round(ha_close, precision, ha_close)
 
     ha_open[0] = open[0]
     ha_high[0] = high[0]
     ha_low[0] = low[0]
-
     for i in range(ha_open.shape[0]):
         if i > 0:
-            ha_open[i] = round((ha_open[i - 1] + ha_close[i - 1]) / 2, precision)
-            ha_high[i] = round(max(ha_open[i], ha_close[i], high[i]), precision)
-            ha_low[i] = round(min(ha_open[i], ha_close[i], low[i]), precision)
+            ha_open[i] = (ha_open[i - 1] + ha_close[i - 1]) / 2
+            ha_high[i] = np.maximum(ha_open[i], ha_close[i], high[i])
+            ha_low[i] = np.minimum(ha_open[i], ha_close[i], low[i])
+
+    # Round to precision
+    if precision is not None:
+        for col in range(ha_close.shape[1]):
+            np.round(ha_close[:, col], precision[col], ha_close[:, col])
+            np.round(ha_open[:, col], precision[col], ha_open[:, col])
+            np.round(ha_high[:, col], precision[col], ha_high[:, col])
+            np.round(ha_low[:, col], precision[col], ha_low[:, col])
 
     return ha_open, ha_high, ha_low, ha_close
 
 
-def HeikinAshi(open, high, low, close, precision=2):
+def HA(open, high, low, close, precision=None):
     """
-    This HeikinAshi function expects a precision to round the data to.
-    This is important to keep the HeikinAshi OHLC consistent with the original input
-
-    This function returns OHLC as 4 different arrays. In addition, it return a dataframe with the HA values.
-
-    AUTHOR: Piotr Yordanov --> https://piotryordanov.com
-    """
-    ha_nb = internal_build_ha(open, high, low, close, precision)
-    ha_df = pd.DataFrame(
-        {"Open": ha_nb[0], "High": ha_nb[1], "Low": ha_nb[2], "Close": ha_nb[3]}
-    )
-    return (
-        ha_nb[0],
-        ha_nb[1],
-        ha_nb[2],
-        ha_nb[3],
-        ha_df,
+    USAGE:
+    ha = HA(
+        data.get("Open"),
+        data.get("High"),
+        data.get("Low"),
+        data.get("Close"),
+        precision,
     )
 
-
-### ============================================================================= ###
-###                             USAGE
-### ============================================================================= ###
-def get_precision(prices):
-    number = prices[0]
-    a = len(str(number).split(".")[1])
-    number = prices[1]
-    a1 = len(str(number).split(".")[1])
-    number = prices[2]
-    a2 = len(str(number).split(".")[1])
-    precision = max(a, a1, a2)
-    return precision
-
-
-data = vbt.BinanceData.fetch(
-    ["BTCUSDT"],
-    timeframe="1h",
-)
-open = np.array(data["Open"])
-high = np.array(data["High"])
-low = np.array(data["Low"])
-close = np.array(data["Close"])
-precision = get_precision(close)
-
-HA = HeikinAshi(open, high, low, close, precision)
-HA["Close"]
+    precision is optional.
+    """
+    HA = vbt.IF(
+        input_names=["open", "high", "low", "close"],
+        output_names=["open", "high", "low", "close"],
+    ).with_apply_func(
+        ha_apply_func,
+        var_args=True,
+    )
+    return HA.run(open, high, low, close, precision)
