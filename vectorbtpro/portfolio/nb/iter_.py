34b9f548bd633f82b2fb20609c8e72a5932e0c29
Oleg Polakow <olegpolakow@gmail.com>
2022-11-10 12:54:30 +0100

    More flexible iterative portfolio functions

--- vectorbtpro/portfolio/nb/iter_.py
+++ vectorbtpro/portfolio/nb/iter_.py
@@ -14,42 +14,156 @@ from vectorbtpro.registries.jit_registry import register_jitted
 
 
 @register_jitted
-def select_nb(ctx: tp.NamedTuple, arr: tp.FlexArray) -> tp.Scalar:
-    """Get the current element using flexible indexing given the context's `i` and `col`."""
-    return flex_select_auto_nb(arr, ctx.i, ctx.col, ctx.flex_2d)
+def select_nb(
+    ctx: tp.NamedTuple,
+    arr: tp.FlexArray,
+    i: tp.Optional[int] = None,
+    col: tp.Optional[int] = None,
+    flex_2d: tp.Optional[bool] = None,
+) -> tp.Scalar:
+    """Get the current element using flexible indexing.
+
+    If any of the arguments are None, will use the respective value from the context."""
+    if i is None:
+        _i = ctx.i
+    else:
+        _i = i
+    if col is None:
+        _col = ctx.col
+    else:
+        _col = col
+    if flex_2d is None:
+        _flex_2d = ctx.flex_2d
+    else:
+        _flex_2d = flex_2d
+    return flex_select_auto_nb(arr, _i, _col, _flex_2d)
 
 
 @register_jitted
-def group_select_nb(ctx: tp.NamedTuple, arr: tp.FlexArray) -> tp.Scalar:
-    """Get the current element using flexible indexing given the context's `i` and `group`."""
-    return flex_select_auto_nb(arr, ctx.i, ctx.group, ctx.flex_2d)
+def select_from_col_nb(
+    ctx: tp.NamedTuple,
+    col: int,
+    arr: tp.FlexArray,
+    i: tp.Optional[int] = None,
+    flex_2d: tp.Optional[bool] = None,
+) -> tp.Scalar:
+    """Get the current element from a specific column using flexible indexing.
+
+    If any of the arguments are None, will use the respective value from the context."""
+    if i is None:
+        _i = ctx.i
+    else:
+        _i = i
+    if flex_2d is None:
+        _flex_2d = ctx.flex_2d
+    else:
+        _flex_2d = flex_2d
+    return flex_select_auto_nb(arr, _i, col, _flex_2d)
 
 
 @register_jitted
-def select_from_col_nb(ctx: tp.NamedTuple, col_or_group: int, arr: tp.FlexArray) -> tp.Scalar:
-    """Get the current element using flexible indexing given a column/group and the context's `i`."""
-    return flex_select_auto_nb(arr, ctx.i, col_or_group, ctx.flex_2d)
+def iter_above_nb(
+    ctx: tp.NamedTuple,
+    arr1: tp.FlexArray,
+    arr2: tp.FlexArray,
+    i: tp.Optional[int] = None,
+    col: tp.Optional[int] = None,
+    flex_2d: tp.Optional[bool] = None,
+) -> bool:
+    """Call `vectorbtpro.generic.nb.iter_.iter_above_nb` on the context.
+
+    If any of the arguments are None, will use the respective value from the context."""
+    if i is None:
+        _i = ctx.i
+    else:
+        _i = i
+    if col is None:
+        _col = ctx.col
+    else:
+        _col = col
+    if flex_2d is None:
+        _flex_2d = ctx.flex_2d
+    else:
+        _flex_2d = flex_2d
+    return _iter_above_nb(arr1, arr2, _i, _col, _flex_2d)
 
 
 @register_jitted
-def iter_above_nb(c: tp.NamedTuple, arr1: tp.FlexArray, arr2: tp.FlexArray) -> bool:
-    """Call `vectorbtpro.generic.nb.iter_.iter_above_nb` on the context."""
-    return _iter_above_nb(arr1, arr2, c.i, c.col, c.flex_2d)
+def iter_below_nb(
+    ctx: tp.NamedTuple,
+    arr1: tp.FlexArray,
+    arr2: tp.FlexArray,
+    i: tp.Optional[int] = None,
+    col: tp.Optional[int] = None,
+    flex_2d: tp.Optional[bool] = None,
+) -> bool:
+    """Call `vectorbtpro.generic.nb.iter_.iter_below_nb` on the context.
+
+    If any of the arguments are None, will use the respective value from the context."""
+    if i is None:
+        _i = ctx.i
+    else:
+        _i = i
+    if col is None:
+        _col = ctx.col
+    else:
+        _col = col
+    if flex_2d is None:
+        _flex_2d = ctx.flex_2d
+    else:
+        _flex_2d = flex_2d
+    return _iter_below_nb(arr1, arr2, _i, _col, _flex_2d)
 
 
 @register_jitted
-def iter_below_nb(c: tp.NamedTuple, arr1: tp.FlexArray, arr2: tp.FlexArray) -> bool:
-    """Call `vectorbtpro.generic.nb.iter_.iter_below_nb` on the context."""
-    return _iter_below_nb(arr1, arr2, c.i, c.col, c.flex_2d)
+def iter_crossed_above_nb(
+    ctx: tp.NamedTuple,
+    arr1: tp.FlexArray,
+    arr2: tp.FlexArray,
+    i: tp.Optional[int] = None,
+    col: tp.Optional[int] = None,
+    flex_2d: tp.Optional[bool] = None,
+) -> bool:
+    """Call `vectorbtpro.generic.nb.iter_.iter_crossed_above_nb` on the context.
+
+    If any of the arguments are None, will use the respective value from the context."""
+    if i is None:
+        _i = ctx.i
+    else:
+        _i = i
+    if col is None:
+        _col = ctx.col
+    else:
+        _col = col
+    if flex_2d is None:
+        _flex_2d = ctx.flex_2d
+    else:
+        _flex_2d = flex_2d
+    return _iter_crossed_above_nb(arr1, arr2, _i, _col, _flex_2d)
 
 
 @register_jitted
-def iter_crossed_above_nb(c: tp.NamedTuple, arr1: tp.FlexArray, arr2: tp.FlexArray) -> bool:
-    """Call `vectorbtpro.generic.nb.iter_.iter_crossed_above_nb` on the context."""
-    return _iter_crossed_above_nb(arr1, arr2, c.i, c.col, c.flex_2d)
+def iter_crossed_below_nb(
+    ctx: tp.NamedTuple,
+    arr1: tp.FlexArray,
+    arr2: tp.FlexArray,
+    i: tp.Optional[int] = None,
+    col: tp.Optional[int] = None,
+    flex_2d: tp.Optional[bool] = None,
+) -> bool:
+    """Call `vectorbtpro.generic.nb.iter_.iter_crossed_below_nb` on the context.
 
-
-@register_jitted
-def iter_crossed_below_nb(c: tp.NamedTuple, arr1: tp.FlexArray, arr2: tp.FlexArray) -> bool:
-    """Call `vectorbtpro.generic.nb.iter_.iter_crossed_below_nb` on the context."""
-    return _iter_crossed_below_nb(arr1, arr2, c.i, c.col, c.flex_2d)
+    If any of the arguments are None, will use the respective value from the context."""
+    if i is None:
+        _i = ctx.i
+    else:
+        _i = i
+    if col is None:
+        _col = ctx.col
+    else:
+        _col = col
+    if flex_2d is None:
+        _flex_2d = ctx.flex_2d
+    else:
+        _flex_2d = flex_2d
+    return _iter_crossed_below_nb(arr1, arr2, _i, _col, _flex_2d)
