3546693192cd91ec5309b7b8d7ba17bb171d04cb
Oleg Polakow <olegpolakow@gmail.com>
2024-03-11 20:22:45 +0100

    Profiling decorators

--- vectorbtpro/utils/profiling.py
+++ vectorbtpro/utils/profiling.py
@@ -5,6 +5,7 @@
 import tracemalloc
 from datetime import timedelta
 from timeit import default_timer, Timer as Timer_timeit
+from functools import wraps, partial
 
 import humanize
 
@@ -12,8 +13,11 @@ from vectorbtpro import _typing as tp
 
 __all__ = [
     "Timer",
+    "with_timer",
     "timeit",
+    "with_timeit",
     "MemTracer",
+    "with_memtracer",
 ]
 
 TimerT = tp.TypeVar("TimerT", bound="Timer")
@@ -29,8 +33,8 @@ class Timer:
         >>> with vbt.Timer() as timer:
         >>>     sleep(1)
 
-        >>> timer.elapsed()
-        '1.01 seconds'
+        >>> print(timer.elapsed())
+        1.01 seconds
 
         >>> timer.elapsed(readable=False)
         datetime.timedelta(seconds=1, microseconds=5110)
@@ -73,8 +77,66 @@ class Timer:
         self._end_time = default_timer()
 
 
+def with_timer(
+    *args,
+    timer_kwargs: tp.KwargsLike = None,
+    elapsed_kwargs: tp.KwargsLike = None,
+    print_func: tp.Optional[tp.Callable] = None,
+    print_format: tp.Optional[str] = None,
+    print_kwargs: tp.KwargsLike = None,
+) -> tp.Callable:
+    """Decorator to run a function with `Timer`."""
+
+    if timer_kwargs is None:
+        timer_kwargs = {}
+    if elapsed_kwargs is None:
+        elapsed_kwargs = {}
+    if print_func is None:
+        print_func = print
+    if print_format is None:
+        print_format = "{func_name} in {elapsed}"
+    if print_kwargs is None:
+        print_kwargs = {}
+
+    def decorator(func: tp.Callable) -> tp.Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> tp.Any:
+            with Timer(**timer_kwargs) as timer:
+                out = func(*args, **kwargs)
+            elapsed = timer.elapsed(**elapsed_kwargs)
+            print_func(print_format.format(
+                func_name=func.__qualname__,
+                elapsed=elapsed,
+            ), **print_kwargs)
+            return out
+
+        return wrapper
+
+    if len(args) == 0:
+        return decorator
+    elif len(args) == 1:
+        return decorator(args[0])
+    raise ValueError("Either function or keyword arguments must be passed")
+
+
 def timeit(func: tp.Callable, readable: bool = True, **kwargs) -> tp.Union[str, timedelta]:
-    """Run `timeit` on a function."""
+    """Run `timeit` on a function.
+
+    Usage:
+        ```pycon
+        >>> from vectorbtpro import *
+
+        >>> def my_func():
+        ...     sleep(1)
+
+        >>> elapsed = vbt.timeit(my_func)
+        >>> print(elapsed)
+        1.01 seconds
+
+        >>> vbt.timeit(my_func, readable=False)
+        datetime.timedelta(seconds=1, microseconds=1870)
+        ```
+    """
     timer = Timer_timeit(stmt=func)
     number, time_taken = timer.autorange()
     elapsed = time_taken / number
@@ -84,6 +146,43 @@ def timeit(func: tp.Callable, readable: bool = True, **kwargs) -> tp.Union[str, 
             kwargs["minimum_unit"] = "seconds" if elapsed >= 1 else "milliseconds"
         return humanize.precisedelta(elapsed_delta, **kwargs)
     return elapsed_delta
+
+
+def with_timeit(
+    *args,
+    timeit_kwargs: tp.KwargsLike = None,
+    print_func: tp.Optional[tp.Callable] = None,
+    print_format: tp.Optional[str] = None,
+    print_kwargs: tp.KwargsLike = None,
+) -> tp.Callable:
+    """Decorator to run a function with `timeit`."""
+
+    if timeit_kwargs is None:
+        timeit_kwargs = {}
+    if print_func is None:
+        print_func = print
+    if print_format is None:
+        print_format = "{func_name} in {elapsed} on average"
+    if print_kwargs is None:
+        print_kwargs = {}
+
+    def decorator(func: tp.Callable) -> tp.Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> tp.Any:
+            elapsed = timeit(partial(func, *args, **kwargs), **timeit_kwargs)
+            print_func(print_format.format(
+                func_name=func.__qualname__,
+                elapsed=elapsed,
+            ), **print_kwargs)
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    if len(args) == 0:
+        return decorator
+    elif len(args) == 1:
+        return decorator(args[0])
+    raise ValueError("Either function or keyword arguments must be passed")
 
 
 MemTracerT = tp.TypeVar("MemTracerT", bound="MemTracer")
@@ -99,8 +198,8 @@ class MemTracer:
         >>> with vbt.MemTracer() as tracer:
         >>>     np.random.uniform(size=1000000)
 
-        >>> tracer.peak_usage()
-        '8.0 MB'
+        >>> print(tracer.peak_usage())
+        8.0 MB
 
         >>> tracer.peak_usage(readable=False)
         8005360
@@ -143,3 +242,45 @@ class MemTracer:
     def __exit__(self, *args) -> None:
         self._final_usage, self._peak_usage = tracemalloc.get_traced_memory()
         tracemalloc.stop()
+
+
+def with_memtracer(
+    *args,
+    memtracer_kwargs: tp.KwargsLike = None,
+    usage_kwargs: tp.KwargsLike = None,
+    print_func: tp.Optional[tp.Callable] = None,
+    print_format: tp.Optional[str] = None,
+    print_kwargs: tp.KwargsLike = None,
+) -> tp.Callable:
+    """Decorator to run a function with `MemTracer`."""
+
+    if memtracer_kwargs is None:
+        memtracer_kwargs = {}
+    if usage_kwargs is None:
+        usage_kwargs = {}
+    if print_func is None:
+        print_func = print
+    if print_format is None:
+        print_format = "{func_name} with peak usage of {peak_usage} and final usage of {final_usage}"
+    if print_kwargs is None:
+        print_kwargs = {}
+
+    def decorator(func: tp.Callable) -> tp.Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> tp.Any:
+            with MemTracer(**memtracer_kwargs) as memtracer:
+                out = func(*args, **kwargs)
+            print_func(print_format.format(
+                func_name=func.__qualname__,
+                peak_usage=memtracer.peak_usage(**usage_kwargs),
+                final_usage=memtracer.final_usage(**usage_kwargs),
+            ), **print_kwargs)
+            return out
+
+        return wrapper
+
+    if len(args) == 0:
+        return decorator
+    elif len(args) == 1:
+        return decorator(args[0])
+    raise ValueError("Either function or keyword arguments must be passed")
